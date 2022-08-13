b87f24e0cbb2f5542a3fb62563dc28d4126e8fc0
Reneilson <reneilson22@gmail.com>
2022-08-13 13:59:37 -0300

    major design update and optimization

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
-const alertModal = document.querySelector('.alert__modal')
 const buttonSubmit = document.querySelectorAll('.button__submit')
 const forms = document.querySelectorAll('.form')
+const list = document.querySelectorAll('.list')
+const sumButtons = document.querySelectorAll('.sum__count__button')
 const imgs = document.querySelectorAll('.pokemon__image')
 const incomingPokemons = document.querySelectorAll('.input__search')
+const lifePokemon = document.querySelectorAll('.life__pokemon')
 const namePokemon = document.querySelectorAll('.name__pokemon')
 const typePokemon = document.querySelectorAll('.type__pokemon')
 const idPokemon = document.querySelectorAll('.id__pokemon')
-const lifePokemon = document.querySelectorAll('.life__pokemon')
 const attackPokemon = document.querySelectorAll('.attack__pokemon')
+const especialAttackPokemon = document.querySelectorAll('.especial__attack__pokemon')
 const defesePokemon = document.querySelectorAll('.defese__pokemon')
+const especialDefesePokemon = document.querySelectorAll('.especial__defese__pokemon')
 const speedPokemon = document.querySelectorAll('.speed__pokemon')
-const totalPokemon = document.querySelectorAll('.total__pokemon')
-const rangeStats = document.querySelectorAll('.range__stats')
+const locationAreaPokemon = document.querySelectorAll('.where__to__find')
+const seePokemon = document.querySelectorAll('.see__pokemon')
+const alertModal = document.querySelector('.alert__modal')
 
 const fetchPokemon = async (pokemon) => {
-  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`)
+  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${String(pokemon).toLowerCase()}`)
   
   if(APIResponse.status === 200){
     const data = await APIResponse.json()
@@ -26,55 +30,148 @@ const fetchPokemon = async (pokemon) => {
   }
 }
 
-const calcPorcentage = (firstValue, secondValue)=>{
-  let totalValue = firstValue + secondValue
-  let porcentageValue = parseInt((100 * firstValue) / totalValue)
-  return porcentageValue
-}
+const handleCardPokemon = {
+  cardInfo: (index, data)=>{
+    namePokemon[index].innerText = data.name
+    idPokemon[index].innerText = `#${data.id}`
+    imgs[index].src = data.sprites.versions['generation-v']['black-white'].animated.front_default
+    lifePokemon[index].innerText = `vida: ${data.stats[0].base_stat}`
+    attackPokemon[index].innerText = `ataque: ${data.stats[1].base_stat}`
+    defesePokemon[index].innerText = `defesa: ${data.stats[2].base_stat}`
+    especialAttackPokemon[index].innerText = `ataque especial: ${data.stats[3].base_stat}`
+    especialDefesePokemon[index].innerText = `defesa especial: ${data.stats[4].base_stat}`
+    speedPokemon[index].innerText = `velocidade: ${data.stats[5].base_stat}`
+  },
+  fromInput: async (e)=>{
+    e.preventDefault()
+    let index = +e.target.dataset.index
+    let value = incomingPokemons[index].value
+    incomingPokemons[index].value = ''
+    const data = await fetchPokemon(value)
+    const dataLocation = await fetch(`https://pokeapi.co/api/v2/pokemon/${data.id}/encounters`)
+    let location = await dataLocation.json()
+ 
+    handleCardPokemon.cardInfo(index, data)
+  
+    typePokemon[index].querySelectorAll('.type').forEach(type => type.remove())
+    for (let i = 0; i < data.types.length; i++) {
+      const type = document.createElement("p");
+      type.innerText = data.types[i].type.name;
+      type.className = `type ${data.types[i].type.name}`;
+      
+      typePokemon[index].appendChild(type)
+    }
 
-const compareStatuses = ()=>{
-  if(idPokemon[0].innerText.length > 1 && idPokemon[1].innerText.length > 1){
-    let firstPokemonLife = Number(lifePokemon[0].innerText.replace('vida: ',''))
-    let secondPokemonLife = Number(lifePokemon[1].innerText.replace('vida: ',''))
-    
-    let firstPokemonAttack = Number(attackPokemon[0].innerText.replace('ataque: ',''))
-    let secondPokemonAttack = Number(attackPokemon[1].innerText.replace('ataque: ',''))
+    locationAreaPokemon[index].innerText = ''
+    if(location.length > 0){
+      for(let i = 0; i < location.length; i++){
+        const locationArea = document.createElement('li')
+        locationArea.innerText = `${location[i].location_area.name.replace(/-/g, ' ')} /`
+        locationArea.className = 'location'
+        locationAreaPokemon[index].appendChild(locationArea)
+      }
+    }else{
+      locationAreaPokemon[index].innerText = 'somente evoluindo'
+    }
+  },
+  fromList: async (e)=>{
+    let index = +e.target.querySelector('.list__id__pokemon').innerText.replace('#','')
+    const data = await fetchPokemon(index)
+    const dataLocation = await fetch(`https://pokeapi.co/api/v2/pokemon/${index}/encounters`)
+    let location = await dataLocation.json()
+    let id = e.target.dataset.id
 
-    let firstPokemonDefese = Number(defesePokemon[0].innerText.replace('defesa: ',''))
-    let secondPokemonDefese = Number(defesePokemon[1].innerText.replace('defesa: ',''))
-    
-    let firstPokemonSpeed = Number(speedPokemon[0].innerText.replace('velocidade: ',''))
-    let secondPokemonSpeed = Number(speedPokemon[1].innerText.replace('velocidade: ',''))
+    list[id].querySelectorAll('.container__pokemon').forEach(container => container.classList.remove('active'))
+    e.target.classList.add('active')
 
-    let firstPokemonTotal = Number(totalPokemon[0].innerText.replace('total: ',''))
-    let secondPokemonTotal = Number(totalPokemon[1].innerText.replace('total: ',''))
+    handleCardPokemon.cardInfo(id, data)
 
-    rangeStats[0].style.background = `linear-gradient(90deg, #666 ${calcPorcentage(firstPokemonLife, secondPokemonLife)}%, #fff 0%)`
-    rangeStats[1].style.background = `linear-gradient(90deg, #666 ${calcPorcentage(firstPokemonAttack, secondPokemonAttack)}%, #fff 0%)`
-    rangeStats[2].style.background = `linear-gradient(90deg, #666 ${calcPorcentage(firstPokemonDefese, secondPokemonDefese)}%, #fff 0%)`
-    rangeStats[3].style.background = `linear-gradient(90deg, #666 ${calcPorcentage(firstPokemonSpeed, secondPokemonSpeed)}%, #fff 0%)`
-    rangeStats[4].style.background = `linear-gradient(90deg, #666 ${calcPorcentage(firstPokemonTotal, secondPokemonTotal)}%, #fff 0%)`
+    typePokemon[id].querySelectorAll('.type').forEach(type => type.remove())
+
+    for (let i = 0; i < data.types.length; i++) {
+      const type = document.createElement("p");
+      type.innerText = data.types[i].type.name;
+      type.className = `type ${data.types[i].type.name}`;
+      
+      typePokemon[id].appendChild(type)
+    }
+
+    locationAreaPokemon[id].innerText = ''
+    if(location.length > 0){
+      for(let i = 0; i < location.length; i++){
+        const locationArea = document.createElement('li')
+        locationArea.innerText = `${location[i].location_area.name.replace(/-/g, ' ')} /`
+        locationArea.className = 'location'
+        locationAreaPokemon[id].appendChild(locationArea)
+      }
+    }else{
+      locationAreaPokemon[id].innerText = 'somente evoluindo'
+    }
   }
 }
 
-const handleForm = async (event)=>{
-  event.preventDefault()
-  const index = event.target.dataset.index
-  const data = await fetchPokemon(incomingPokemons[index].value)
-  let totalPoints = data.stats[0].base_stat + data.stats[1].base_stat + data.stats[2].base_stat + data.stats[5].base_stat
+const handleListPokemon = async (countInit, countMap, position) => {
+  countMap = +countInit + +countMap;
+  for (let i = 1; countInit < countMap; i++) {
+   countInit++;
+   countLast = countMap;
+   const data = await fetchPokemon(countInit);
+   const liPokemon = document.createElement('li')
+   
+   liPokemon.className = 'container__pokemon'
+   liPokemon.setAttribute('data-id',position)
+   liPokemon.innerHTML =  `
+   <img class="list__pokemon__image" src="${data.sprites.versions["generation-v"]["black-white"].animated.front_default}"/>
+   <div class="desc">
+   <p class="list__pokemon__name">${data.name}</p>
+   <div class="container__type"></div>
+   </div>
+   <p class="list__id__pokemon">#${data.id}</p>
+   `
+   for (let i = 0; i < data.types.length; i++) {
+     const typePokemon = document.createElement("p");
+     
+      typePokemon.innerText = data.types[i].type.name;
+      typePokemon.className = `type ${data.types[i].type.name}`;
+      
+      liPokemon.querySelector('.container__type').appendChild(typePokemon)
+    }
+    
+    liPokemon.addEventListener('click', handleCardPokemon.fromList)
+    list[position].appendChild(liPokemon);
+  }
+ };
+ 
+ const handleSum = ({ target }) => {
+  const lastId = list[+target.dataset.index];
+  const formatedId = lastId.lastChild.querySelector('.list__id__pokemon').innerText.replace("#", "");
+  const countInit = formatedId;
+  const countLast = +target.dataset.count;
+  let startPosition = +target.dataset.id
+  let lastPosition
   
-  incomingPokemons[index].value = ''
-  namePokemon[index].innerText = data.name
-  idPokemon[index].innerText = `#${data.id}`
-  typePokemon[index].innerText = data.types[0].type.name
-  typePokemon[index].className = `type__pokemon ${data.types[0].type.name}`
-  imgs[index].src = data.sprites.versions['generation-v']['black-white'].animated.front_default
-  lifePokemon[index].innerText = `vida: ${data.stats[0].base_stat}`
-  attackPokemon[index].innerText = `ataque: ${data.stats[1].base_stat}`
-  defesePokemon[index].innerText = `defesa: ${data.stats[2].base_stat}`
-  speedPokemon[index].innerText = `velocidade: ${data.stats[5].base_stat}`
-  totalPokemon[index].innerText = `total: ${totalPoints}`
-  compareStatuses()
+  if(+target.dataset.index === 0){
+    lastPosition = 2
+  }else{
+    lastPosition = 4
+  }
+  
+  handleListPokemon(countInit, countLast, +target.dataset.index);
+
+  for(let i = startPosition; i < lastPosition; i++){
+    sumButtons[i].disabled = true;
+    setTimeout(()=> sumButtons[i].disabled = false, `${countLast}99`)
+  }
+};
+
+const seePokemonOnMobile = ({ target })=>{
+  document.querySelectorAll('.card__pokemon')[target.dataset.index].classList.toggle('see__pokemon__active')
+  target.innerText === 'mostrar pokémon' ? target.innerText = 'esconder pokémon' : target.innerText = 'mostrar pokémon'
 }
 
-forms.forEach(form => form.addEventListener('submit', handleForm))
+ handleListPokemon(0,10,0)
+ handleListPokemon(0,10,1)
+
+forms.forEach(form => form.addEventListener('submit', handleCardPokemon.fromInput))
+sumButtons.forEach(button => button.addEventListener('click', handleSum))
+seePokemon.forEach(buttonSee => buttonSee.addEventListener('click', seePokemonOnMobile))
